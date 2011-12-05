a8020a046f4a2b1ba8d7a5b43bff669cf5704c2e
Fernando Alonso <krakatoa1987@gmail.com>
2011-12-04 23:45:49 -0300

    Include sentence

--- lib/rzmq_compat.rb
+++ lib/rzmq_compat.rb
@@ -1,4 +1,13 @@
 module RZMQMCompat
+  def self.included(klass)
+    klass.instance_eval do
+      %w(recv).each do |m|
+        alias_method :"#{m}_without_raise", m.to_sym
+        alias_method m.to_sym, :"#{m}_with_raise"
+      end
+    end
+  end
+
   def recv_with_raise(msg, flags = 0)
     recv_without_raise(msg, flags)
   end
