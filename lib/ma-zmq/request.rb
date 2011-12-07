eba32e9f059cb9e36fdf28365d5bfaca64a3d12c
Fernando Alonso <krakatoa1987@gmail.com>
2011-12-06 23:24:50 -0300

    Arranged some code into MaZMQ::Socket

--- lib/ma-zmq/request.rb
+++ lib/ma-zmq/request.rb
@@ -2,26 +2,5 @@ module MaZMQ
   class Request < MaZMQ::Socket
     # mergear con RoundRobin
     socket_type ZMQ::REQ
-
-    def initialize()
-      @socket = MaZMQ::context.socket(@@socket_type)
-    end
-
-    def connect(protocol, address, port)
-      return false if not MaZMQ::Socket.valid_protocol?(protocol)
-
-      zmq_address = "#{protocol.to_s}://#{address}:#{port.to_s}"
-      @socket.connect(zmq_address)
-    end
-
-    def send_string(msg)
-      @socket.send_string(msg)
-    end
-
-    def recv_string
-      msg = ''
-      @socket.recv_string(msg)
-      msg
-    end
   end
 end
--- lib/ma-zmq/socket.rb
+++ lib/ma-zmq/socket.rb
@@ -6,6 +6,36 @@ module MaZMQ
       @@socket_type = socket_type
     end
 
+    def initialize()
+      @socket = MaZMQ::context.socket(@@socket_type)
+      #@em_handler = bla
+    end
+
+    def connect(protocol, address, port)
+      return false if not MaZMQ::Socket.valid_protocol?(protocol)
+
+      zmq_address = "#{protocol.to_s}://#{address}:#{port.to_s}"
+      @socket.connect(zmq_address)
+    end
+
+    def send_string(msg)
+      @socket.send_string(msg)
+    end
+
+    def recv_string
+      msg = ''
+      @socket.recv_string(msg)
+      msg
+    end
+
+    def on_read(&block)
+      #OwnHandler.pass(&block)
+    end
+
+    def on_write(&block)
+      #OwnHandler.pass(&block)
+    end
+
     protected
     def self.valid_protocol?(protocol)
       @@protocols.include? protocol
