102bb3d397bd6e6d32848b3a5e4dda91970954fb
Fernando Alonso <krakatoa1987@gmail.com>
2011-12-17 01:05:54 -0300

    Added @state on Reply object

--- lib/ma-zmq/reply.rb
+++ lib/ma-zmq/reply.rb
@@ -1,11 +1,34 @@
 module MaZMQ
   class Reply < MaZMQ::SocketHandler
-    # mergear con RoundRobin
-    #socket_type ZMQ::REP
+    attr_reader :state
 
     def initialize(use_eventmachine=true)
       @socket_type = ZMQ::REP
       super(use_eventmachine)
+    end
+
+    def recv_string
+      case @state
+        when :idle
+          msg = super
+          if msg and not msg.empty?
+            @state = :reply
+          end
+          return msg
+        else
+          return false
+      end
+    end
+
+    def send_string(msg)
+      case @state
+        when :reply
+          resp = super(msg)
+          @state = :idle
+          return resp
+        else
+          return false
+      end
     end
   end
 end
